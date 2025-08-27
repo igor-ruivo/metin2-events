8dce083e808a40999b681a7b31a736c56d7f31bb
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-08-27 04:42:08 +0100

    add next

--- src/bin/register.ts
+++ src/bin/register.ts
@@ -2,29 +2,61 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// Load environment variables
 const config = {
 	token: process.env.DISCORD_TOKEN,
 	clientId: process.env.DISCORD_CLIENT_ID,
-	guildId: process.env.DISCORD_GUILD_ID,
-	channelId: process.env.DISCORD_CHANNEL_ID,
 };
 
-const main = async () => {
-	const BOT_TOKEN = config.token;
-	const CLIENT_ID = config.clientId;
+void (async () => {
+	// Pega todos os comandos globais
+	const commands: Array<{
+		name: string;
+		id: string;
+		options: Array<{
+			name: string;
+			choices: Array<{ name: string; value: string }>;
+		}>;
+	}> = (await (
+		await fetch(
+			`https://discord.com/api/v10/applications/${config.clientId}/commands`,
+			{ headers: { Authorization: `Bot ${config.token}` } }
+		)
+	).json()) as Array<{
+		name: string;
+		id: string;
+		options: Array<{
+			name: string;
+			choices: Array<{ name: string; value: string }>;
+		}>;
+	}>;
 
-	const res = await fetch(
-		`https://discord.com/api/v10/applications/${CLIENT_ID}/commands`,
+	const command = commands.find((c) => c.name === 'events');
+	if (!command) {
+		console.log('Couldnt find command');
+		return;
+	}
+	const option = command.options.find(
+		(o: { name: string }) => o.name === 'period'
+	);
+
+	if (!option) {
+		console.log('couldnt find option');
+		return;
+	}
+
+	option.choices.push({ name: 'next', value: 'next' });
+
+	await fetch(
+		`https://discord.com/api/v10/applications/${config.clientId}/commands/${command.id}`,
 		{
+			method: 'PATCH',
 			headers: {
-				Authorization: `Bot ${BOT_TOKEN}`,
+				'Authorization': `Bot ${config.token}`,
+				'Content-Type': 'application/json',
 			},
+			body: JSON.stringify(command),
 		}
 	);
 
-	const ans: unknown = await res.json();
-	console.log(JSON.stringify(ans));
-};
-
-void main();
+	console.log('Comando atualizado!');
+})();
